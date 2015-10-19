9a205f38dcce3cee0f1cf300c5df62a93061105b
ccanete <cyril.canete@insa-lyon.fr>
2015-10-19 09:26:00 +0200

    initial commit bis

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
+"use strict";
+
 var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
+  res.send('Welcome to make-the-search-a-better-place engine!');
+});
+
+app.get('/sayHello', function (req, res) {
   res.send('Hello World!');
 });
 
-app.get('/search', function (req, res) {
-  res.send('Fill the page with results of request ' + req.query.query);
-});
 
 var server = app.listen(3000, function () {
   var host = server.address().address;

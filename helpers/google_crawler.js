1456d6effbe3f0c22b996b9d8a4f0d922f6e6bef
V1C0D3R <V1C0D3R@gmail.com>
2015-10-19 10:21:48 +0200

    Add helpers directory and googleCrawler helper

---
+++ helpers/google_crawler.js
@@ -0,0 +1,30 @@
+"use strict";
+
+var Crawler = require("crawler");
+var url = require('url');
+
+var googleCrawler = new Crawler({
+    maxConnections : 10,
+    // This will be called for each crawled page 
+    callback : function (error, result, $) {
+        // $ is Cheerio by default 
+        //a lean implementation of core jQuery designed specifically for the server 
+        $('h3.r > a').each(function(index, r) {
+            
+            //var toQueueUrl = $(a).attr('href');
+            //c.queue(toQueueUrl);
+            console.log($(r).attr('href'));
+        });
+    }
+});
+
+var getGoogleResult = function(r) {
+    googleCrawler.queue({
+        uri: googleSearch(r)
+    });
+};
+module.exports.getGoogleResult = getGoogleResult;
+
+var googleSearch = function(search) {
+  return 'http://www.google.fr/search?q=' + search + '&num=10';
+};

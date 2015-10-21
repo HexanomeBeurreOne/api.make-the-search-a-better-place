0cda80b0a11817d56ff872465a5a983e4522d75c
QuentinBay <quentin.bayart45430@gmail.com>
2015-10-21 10:13:14 +0200

    Add http call to dbpedia to retrieve URIs from a text

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var express = require('express');
 var app = express();
 var gCrawler = require('./helpers/google_crawler.js');
 
+var spotlight = require('./helpers/spotlight_use.js');
+
 
 app.get('/', function (req, res) {
   res.send('Welcome to make-the-search-a-better-place engine!');
@@ -17,6 +19,18 @@ app.get('/search', function (req, res) {
   	res.send('Fill the page with results of request ' + req.query.q);
   	gCrawler.getGoogleResult(req.query.q);
 });
+
+
+//------------------------------------ Test the use of spotlight
+app.get('/spotlight', function (req, res) {
+	var data = spotlight.spotlightSearch(req.query.text, function(err, results){
+		//Display results which is a URI list
+		res.send('Text tested : ' + results);
+	});
+  	
+  	
+});
+
 
 var server = app.listen(3000, function () {
   var host = server.address().address;

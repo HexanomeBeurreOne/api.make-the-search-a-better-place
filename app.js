bc43b5dc777d8494f31d8de25eb12c7a01cab883
MrBerts <berthier.gllm@gmail.com>
2015-10-21 09:45:23 +0200

    Appel d'une api web pour afficher du texte à partir d'une URL

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var app = express();
 var gCrawler = require('./helpers/google_crawler.js');
+var urlToText = require('./helpers/urlToText.js');
 
 
 app.get('/', function (req, res) {
@@ -21,6 +22,11 @@ app.get('/search', function (req, res) {
   		res.contentType('application/json');
 		res.send(JSON.stringify(links));
   	});
+});
+
+app.get('/getTextfromUrl', function(req, res) {
+	urlToText.getTextFromUrl('https://fr.wikipedia.org/wiki/Lol');
+	res.send('getTextfromUrl');
 });
 
 var server = app.listen(3000, function () {

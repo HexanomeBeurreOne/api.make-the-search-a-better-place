4e52d4461d0095f967736baac0fd519e45188265
V1C0D3R <V1C0D3R@gmail.com>
2015-10-21 10:30:30 +0200

    Change query var again to avoid possible conflicts and secure google callback

--- helpers/google_crawler.js
+++ helpers/google_crawler.js
@@ -9,14 +9,23 @@ var googleCallback = function (error, result, $) {
         //a lean implementation of core jQuery designed specifically for the server
         var uriArray = [];
         $('h3.r > a').each(function(index, a) {
-            var href = $(a).attr('href');
-            var uri = href.replace("/url?q=", "");
-            ///console.log(uri);
-            //var toQueueUrl = $(a).attr('href');
-            //c.queue(toQueueUrl);
-            if (uri.indexOf("http") == 0) {
-                uriArray.push(uri);
-            };
+            var href;
+            if (href = $(a).attr('href')) {
+                var uri;
+    
+                if (href.indexOf("/url?q=") != -1) {
+                    uri = href.replace("/url?q=", "");
+                } else {
+                    uri = href;
+                }
+
+                //var toQueueUrl = $(a).attr('href');
+                //c.queue(toQueueUrl);
+                
+                if (uri.indexOf("http") == 0) {
+                    uriArray.push(uri);
+                };
+            }
         });
         //console.log(uriArray);
         
@@ -29,12 +38,12 @@ var googleCrawler = new Crawler({
     // This will be called for each crawled page 
 });
 
-var getGoogleResult = function(query, numOfResult, appCallback) {
+var getGoogleResult = function(searchQuery, numOfResult, appCallback) {
     //Set app.js callback where array should be returned
     mainCallback = appCallback;
     //Call crawler
     googleCrawler.queue({
-        uri: googleSearch(query, numOfResult),
+        uri: googleSearch(searchQuery, numOfResult),
         callback: googleCallback
     });
 };

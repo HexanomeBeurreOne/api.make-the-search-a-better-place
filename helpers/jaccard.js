4e3f3284032e403a644a813060dc274ab836048e
Adrien Menella <adrien.menella@gmail.com>
2015-10-21 13:14:16 +0200

    jaccard index between two tabs of URI

---
+++ helpers/jaccard.js
@@ -0,0 +1,11 @@
+"use strict"
+
+var calculateJaccardIndex = function(tab1, tab2) {
+	var sameUri = 0;
+	for (var i=0 ; i<tab1.length ; i++) {
+		for (var j = 0; j < tab2.length; j++) {
+			if(tab1[i]==tab2[j]) sameUri++;
+		};
+	};
+	return sameUri/(tab1.length+tab2.length-sameUri);
+}

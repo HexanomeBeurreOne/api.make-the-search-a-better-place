851568f67c6e4b1077ea5f1a45e6024d25a22ca6
QuentinBay <quentin.bayart45430@gmail.com>
2015-11-02 08:26:02 +0100

    Moves the require outside of fonction

--- helpers/sparql.js
+++ helpers/sparql.js
@@ -1,13 +1,13 @@
 //Avoid issues calling non-existing variables
 "use strict";
 
+var sparql = require('jsparql');
+
+var client = new sparql('http://dbpedia.org/sparql', 'http://dbpedia.org');
+
 var sparqlSearch = function(search, callback) {
 		
-		//---------------------------------------------Sparql Call
-		var sparql = require('jsparql');
-	 
-		var client = new sparql('http://dbpedia.org/sparql', 'http://dbpedia.org');
-		 
+		//---------------------------------------------Sparql query
 		client.query(' SELECT * WHERE { ?s ?p ?o. FILTER(?s in ('+search+')) } LIMIT 100', function (err, results) {
 			if (err) throw err;
 			  

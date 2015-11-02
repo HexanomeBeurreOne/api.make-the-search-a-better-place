578092bdd67b637afdf6ff147b31578f59c195c3
V1C0D3R <V1C0D3R@gmail.com>
2015-11-02 08:54:02 +0100

    Resolve 'page not found' problem and add comments

--- helpers/google_crawler.js
+++ helpers/google_crawler.js
@@ -5,16 +5,20 @@ var url = require('url');
 
 var mainCallback;
 var googleCallback = function (error, result, $) {
-        // $ is Cheerio by default
-        //a lean implementation of core jQuery designed specifically for the server
         var uriArray = [];
+        //URIs are in 'a' DOM element in 'h3' DOM of class 'r'
         $('h3.r > a').each(function(index, a) {
             var href;
+            //Get link in 'a' DOM element
             if (href = $(a).attr('href')) {
                 var uri;
-    
+                
+                //Crop href result to find URI
                 if (href.indexOf("/url?q=") != -1) {
                     uri = href.replace("/url?q=", "");
+                    if (uri.indexOf("&sa=") != -1) {
+                        uri = uri.substring(0, uri.indexOf("&sa="));
+                    }
                 } else {
                     uri = href;
                 }
@@ -22,6 +26,7 @@ var googleCallback = function (error, result, $) {
                 //var toQueueUrl = $(a).attr('href');
                 //c.queue(toQueueUrl);
                 
+                //Push in array every link which begin with 'http'
                 if (uri.indexOf("http") == 0) {
                     uriArray.push(uri);
                 };
@@ -49,6 +54,7 @@ var getGoogleResult = function(searchQuery, numOfResult, appCallback) {
 };
 module.exports.getGoogleResult = getGoogleResult;
 
+//Create Google URI string with query and number of results wanted
 var googleSearch = function(query, numOfResult) {
   return 'http://www.google.fr/search?q=' + query + '&num=' + numOfResult;
 };

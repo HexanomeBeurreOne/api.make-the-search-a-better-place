a5b8fccb5be71eeed66bcd4adf645af995a788a0
V1C0D3R <V1C0D3R@gmail.com>
2015-11-03 14:34:18 +0100

    Add fake D3JS graph

--- views/js/graph.js
+++ views/js/graph.js
@@ -1,19 +1,20 @@
-var width = 960,
-    height = 500;
+window.onload = function runD3code() {
+  var width = 1000,
+      height = 700;
 
-var color = d3.scale.category20();
+  var color = d3.scale.category20();
 
-var force = d3.layout.force()
-    .charge(-120)
-    .linkDistance(30)
-    .size([width, height]);
+  var force = d3.layout.force()
+      .charge(-120)
+      .linkDistance(30)
+      .size([width, height]);
 
-var svg = d3.select("graphBody").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+  var svg = d3.select("#graph").append("svg")
+      .attr("width", width)
+      .attr("height", height);
 
-d3.json("views/js/miserables.json", function(error, graph) {
-  if (error) throw error;
+  d3.json("views/js/miserables.json", function(error, graph) {
+    if (error) throw error;
 
   force
       .nodes(graph.nodes)
@@ -26,24 +27,38 @@ d3.json("views/js/miserables.json", function(error, graph) {
       .attr("class", "link")
       .style("stroke-width", function(d) { return Math.sqrt(d.value); });
 
-  var node = svg.selectAll(".node")
-      .data(graph.nodes)
-    .enter().append("circle")
+  var gnodes = svg.selectAll('g.gnode')
+     .data(graph.nodes)
+     .enter()
+     .append('g')
+     .classed('gnode', true);
+    
+  var node = gnodes.append("circle")
       .attr("class", "node")
       .attr("r", 5)
       .style("fill", function(d) { return color(d.group); })
       .call(force.drag);
 
-  node.append("title")
+  var labels = gnodes.append("text")
+      .attr("dx", 12)
+      .attr("dy", ".35em")
+      .attr("font-size", "10px")
       .text(function(d) { return d.name; });
 
+  console.log(labels);
+    
   force.on("tick", function() {
     link.attr("x1", function(d) { return d.source.x; })
         .attr("y1", function(d) { return d.source.y; })
         .attr("x2", function(d) { return d.target.x; })
         .attr("y2", function(d) { return d.target.y; });
 
-    node.attr("cx", function(d) { return d.x; })
-        .attr("cy", function(d) { return d.y; });
+    gnodes.attr("transform", function(d) { 
+        return 'translate(' + [d.x, d.y] + ')'; 
+    });
+      
+    
+      
   });
-});
+  });
+}
